2d4361c33539fa20331e6c7cf3d8bd243bc40b66
Amalay <84455469+amalay@users.noreply.github.com>
2021-09-14 14:41:40 +0530

    Updated

---
+++ DotNet/Helpers/Amalay.Framework/Helpers/Security/CertificateHelper.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Amalay.Framework
+{
+    public class CertificateHelper
+    {
+        private string fileName = "CertificateHelper.cs";
+
+        #region "Singleton"
+
+        private static readonly CertificateHelper instance = new CertificateHelper();
+
+        private CertificateHelper() { }
+
+        public static CertificateHelper Instance
+        {
+            get
+            {
+                return instance;
+            }
+        }
+
+        #endregion
+
+        public bool InstallCertificate(string certificateDirectoryName, string cetificateFileName, string cetificatePassword, string powershellScriptFileName)
+        {
+            bool status = false;
+
+            try
+            {
+                string certificateDirectory = System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location) + @"\" + certificateDirectoryName + @"\";
+
+                string powerShellScriptFilePath = certificateDirectory + powershellScriptFileName;
+
+                if (System.IO.File.Exists(powerShellScriptFilePath))
+                {
+                    string certificateFilePath = certificateDirectory + cetificateFileName;
+
+                    if (System.IO.File.Exists(certificateFilePath))
+                    {
+                        string command = String.Format("/C powershell \"{0}\" -certificateFilePath \"{1}\" -certificatePassword '{2}' ", powerShellScriptFilePath, certificateFilePath, cetificatePassword);
+
+                        System.Diagnostics.Process.Start("cmd", command);
+                        status = true;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
+            return status;
+        }
+
+        public X509Certificate2 GetCertificateByThumbPrint(string thumbPrint, StoreLocation location)
+        {
+            return this.GetCertificate(X509FindType.FindByThumbprint, thumbPrint, location);
+        }
+
+        public X509Certificate2 GetCertificateBySubjectName(string subjectName, StoreLocation location)
+        {
+            return this.GetCertificate(X509FindType.FindBySubjectName, subjectName, location);
+        }
+
+        public string CleanThumbPrint(string thumbPrint)
+        {
+            //replace spaces, non word chars and convert to uppercase
+            return System.Text.RegularExpressions.Regex.Replace(thumbPrint, @"\s|\W", "").ToUpper();
+        }
+
+        private X509Certificate2 GetCertificate(X509FindType findType, string findValue, StoreLocation location)
+        {
+
+            DateTime furthestExpirey = DateTime.MinValue;
+            X509Certificate2 certificate = null;
+            X509Store certStore = new X509Store("My", location);
+
+            try
+            {
+                certStore.Open(OpenFlags.ReadOnly | OpenFlags.OpenExistingOnly);
+
+                X509CertificateCollection certs = certStore.Certificates.Find(findType, findValue, false);
+
+                foreach (X509Certificate2 cert in certs)
+                {
+                    if (cert != null)
+                    {
+                        if (cert.NotAfter >= furthestExpirey)
+                        {
+                            certificate = cert;
+                            furthestExpirey = cert.NotAfter;
+                        }
+                    }
+                }
+            }
+            finally
+            {
+                certStore.Close();
+            }
+
+            return certificate;
+        }
+    }
+}
---
+++ DotNet/Helpers/Amalay.Framework/Helpers/Security/CryptographyHelper.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Amalay.Framework
+{
+    public class CryptographyHelper
+    {
+        private string fileName = "CryptographyHelper.cs";
+
+        #region "Singleton"
+
+        private static readonly CryptographyHelper instance = new CryptographyHelper();
+
+        private CryptographyHelper() { }
+
+        public static CryptographyHelper Instance
+        {
+            get
+            {
+                return instance;
+            }
+        }
+
+        #endregion
+
+        #region "Public Methods"
+
+        /// <summary>
+        /// Method to return the masked value of a string.
+        /// </summary>
+        /// <param name="inputString"></param>
+        /// <returns></returns>
+        public string GetMaskedValue(string inputString)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            if (!string.IsNullOrEmpty(inputString))
+            {
+                var algorithm = System.Security.Cryptography.SHA512.Create();  //or use SHA256.Create();
+                var hashedValue = algorithm.ComputeHash(Encoding.UTF8.GetBytes(inputString));
+
+                if (hashedValue != null && hashedValue.Length > 0)
+                {
+                    foreach (byte b in hashedValue)
+                    {
+                        sb.Append(b.ToString("X2"));
+                    }
+                }
+            }
+
+            return sb.ToString();
+        }
+
+        /// <summary>
+        /// Method to return list of masked value. It accepts key & its value in plain text and return the same key with its masked value.
+        /// </summary>
+        /// <param name="inputList"></param>
+        /// <returns></returns>
+        public Dictionary<string, string> GetMaskedValue(Dictionary<string, string> inputList)
+        {
+            Dictionary<string, string> maskedList = null;
+
+            if (inputList != null && inputList.Count > 0)
+            {
+                maskedList = new Dictionary<string, string>();
+
+                foreach (var item in inputList)
+                {
+                    if (!string.IsNullOrEmpty(item.Value))
+                    {
+                        var algorithm = System.Security.Cryptography.SHA512.Create();  //or use SHA256.Create();
+                        var hashedValue = algorithm.ComputeHash(Encoding.UTF8.GetBytes(item.Value));
+
+                        if (hashedValue != null && hashedValue.Length > 0)
+                        {
+                            StringBuilder sb = new StringBuilder();
+
+                            foreach (byte b in hashedValue)
+                            {
+                                sb.Append(b.ToString("X2"));
+                            }
+
+                            maskedList.Add(item.Key, sb.ToString());
+                        }
+                    }
+                }
+            }
+
+            return maskedList;
+        }
+
+        #endregion
+    }
+}
