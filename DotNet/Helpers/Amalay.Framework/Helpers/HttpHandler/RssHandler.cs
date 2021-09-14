d2ffce8de58e7026f2e7f5274587a4cfdbde0206
Amalay <84455469+amalay@users.noreply.github.com>
2021-09-14 14:47:46 +0530

    Updated

---
+++ DotNet/Helpers/Amalay.Framework/Helpers/HttpHandler/RssHandler.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace Amalay.Framework
+{
+    //public class RssHandler : IHttpHandler
+    //{
+    //    private string fileName = "RssHandler.cs";       
+
+    //    public bool IsReusable
+    //    {            
+    //        get 
+    //        { 
+    //            return false; 
+    //        }
+    //    }
+
+    //    public void ProcessRequest(HttpContext context)
+    //    {
+    //        context.Response.ContentType = "text/xml";
+
+    //        using (XmlWriter xmlWriter = XmlWriter.Create(context.Response.OutputStream))
+    //        {
+    //            xmlWriter.WriteStartDocument();
+    //            xmlWriter.WriteElementString("Rss", "Amalay Http Handler RSS Feed!");
+    //            xmlWriter.WriteEndDocument();
+    //            xmlWriter.Flush();
+    //        }
+    //    }
+    //}
+}
---
+++ DotNet/Helpers/Amalay.Framework/Helpers/HttpModules/LoggingHttpModule.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Amalay.Framework
+{
+    //public class LoggingHttpModule : IHttpModule
+    //{
+    //    public void Dispose()
+    //    {
+
+    //    }
+
+    //    public void Init(HttpApplication context)
+    //    {
+    //        context.LogRequest += Application_LogRequest;
+    //    }
+
+    //    private void Application_LogRequest(object sender, EventArgs e)
+    //    {
+    //        HttpApplication httpApp = sender as HttpApplication;
+    //        HttpContext httpContext = httpApp.Context;
+
+    //        string requestPath = httpContext.Request.Path;
+
+    //        Trace.WriteLine(string.Format("Request Path: {0}", requestPath));
+    //    }
+    //}
+}
